54961cf293a603772ee9bf58c16e5de1a55be9a9
Jodean Nascimento <jodean.nascimento@gmail.com>
2022-11-01 06:38:15 -0300

    app contador

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  // Declarando estado
+  const [count, setCount] = useState(0);
+
+
+  // função incrementar
+  let increment = () => {
+    setCount(count + 1);
+  }
+
+  // função decrementar
+  let decrement = () => {
+    setCount(count - 1);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={styles.numberCount}>{count}</Text>
+
+      <View style={styles.buttonContent}>
+        { (count <= 0) ? ("") : ( <TouchableOpacity style={styles.touchButton} onPress={ decrement }><Text style={styles.textButton}>Descrementar (-)</Text></TouchableOpacity> ) } 
+        <TouchableOpacity style={styles.touchButton} onPress={ increment }><Text style={styles.textButton}>Incrementar (+)</Text></TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -17,4 +39,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  numberCount: {
+    fontSize: 60,
+    color: "#8800ff"
+  },
+
+  buttonContent: {
+    padding: 20,
+    flexDirection: 'row',
+    marginTop: 20
+  },
+
+  touchButton: {
+    width: 200,
+    padding: 10,
+    borderWidth: .4,
+    borderRadius: 4,
+    borderColor: '#8800ff',
+    marginHorizontal: 10
+  },
+
+  textButton: {
+    textAlign: 'center',
+    color: '#8800ff'
+  },
+
 });
